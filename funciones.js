8fef213b1e079345b4a408ceec3ae336820221c8
LionelMazzutti <86435660+LionelMazzutti@users.noreply.github.com>
2023-01-31 03:53:18 -0300

    Proyecto

---
+++ funciones.js
@@ -0,0 +1,51 @@
+
+
+document.getElementById("boton1").addEventListener("click",function() {
+  
+    document.getElementById("datosc").style.display = "block";
+    "block";
+})
+
+document.getElementById("boton2").addEventListener("click",function() {
+  
+    document.getElementById("datose").style.display = "block";
+    "block";
+})
+
+document.getElementById("boton3").addEventListener("click",function() {
+  
+    document.getElementById("datosexp").style.display = "block";
+    "block";
+})
+
+document.getElementById("boton4").addEventListener("click",function() {
+  
+    document.getElementById("datosh").style.display = "block";
+    "block";
+})
+
+document.getElementById("boton11").addEventListener("click",function() {
+  
+    document.getElementById("datosc").style.display = "none";
+    "block";
+})
+
+document.getElementById("boton22").addEventListener("click",function() {
+  
+    document.getElementById("datose").style.display = "none";
+    "block";
+})
+
+document.getElementById("boton33").addEventListener("click",function() {
+  
+    document.getElementById("datosexp").style.display = "none";
+    "block";
+})
+
+document.getElementById("boton44").addEventListener("click",function() {
+  
+    document.getElementById("datosh").style.display = "none";
+    "block";
+})
+
+
